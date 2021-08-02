592631e864433f70d17b9b54a292b6047654c460
Constanzia <83115706+Constanzia@users.noreply.github.com>
2021-08-02 17:01:54 +0800

    Update dubins1.js

--- docs/dubins1.js
+++ docs/dubins1.js
@@ -1,10 +1,13 @@
 
-var i,size1,vmax,vmin
+var size1
+var vmax=37
+var vmin=25
 let k,a,b
 let x,y,r
-let button;
+let button,button2;
 let buttontext
-let g=true
+let g=false
+var i=1
 
 var cir1= {x:65,y:65}
     cir2= {x:cir1.x+90,y:cir1.y}
@@ -36,13 +39,16 @@ buttontext='grid view'
 
    v=random([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0])
  // console.log(v)
-  size1=createSlider(5,45,34)
+  size1=createSlider(5,45,32)
   //size1.position(40, 80);
   size1.addClass("mySliders");
  button = createButton(buttontext);
   //button.position(0, 0);
   button.mousePressed(showGrid);
    button.addClass("myButtons");
+  button2 = createButton('→');
+   button2.addClass("myButtons");
+  button2.mousePressed(changeImage);
 }
 
 function draw() {
@@ -60,28 +66,17 @@ r=size1.value();
   
   
   
-  // circle([x],[y],2*r)
+if(i==1){
 newbase1();
-  //console.log(ssl.x)
 strokeWeight(3)
-  
-  //shape
 draw1();
-  
-  //shape
-  refill1();
- // if(r<=27){
- // empty(cir11.x,cir11.y,2*r); 
-       //   }else{ }
+refill1();
+}else if (i==2){newbase2();
+strokeWeight(3)
+draw2();
+refill2();}
 
-
-
-
-     // }
-    //console.log(cos(PI/8.0)) ;
- // }
-
-   if(g){grid1()}
+if(g){grid1();}
    
 //noLoop()
   
@@ -107,9 +102,14 @@ function showGrid() {
 g=!g;
 buttontext=('show grid')
 }
+function changeImage() {
+if (i<2){i++;}else
+{i=1}
+}
 
 
-function newbase1(){base(cir1.x,cir1.y,2*r);
+function newbase1(){
+  base(cir1.x,cir1.y,2*r);
 base(cir2.x,cir2.y,2*r);
 empty(cir3.x,cir3.y,2*r);
 empty(cir4.x,cir4.y,2*r);
@@ -127,9 +127,7 @@ empty(cir15.x,cir15.y,2*r);
 base(cir16.x,cir16.y,2*r);}
 
 function draw1(){beginShape();
-var vmax=38
-var vmin=25
-                 
+              
 vertex(cir1.x,cir1.y-r);
 vertex(cir2.x,cir2.y-r);
 vertex(cir2.x+r*sin(PI/8.0),cir2.y-r*cos(PI/8.0));
@@ -186,21 +184,129 @@ endShape(CLOSE);}
 function refill1(){empty(cir7.x,cir7.y,2*r); 
 empty(cir12.x,cir12.y,2*r); 
   empty(cir10.x,cir10.y,2*r); }
+function newbase2(){
+
+base(cir1.x,cir1.y,2*r);
+empty(cir2.x,cir2.y,2*r);
+empty(cir3.x,cir3.y,2*r);
+base(cir4.x,cir4.y,2*r);
+base(cir5.x,cir5.y,2*r);
+empty(cir6.x,cir6.y,2*r);
+empty(cir7.x,cir7.y,2*r);
+empty(cir8.x,cir8.y,2*r);
+empty(cir9.x,cir9.y,2*r);
+empty(cir10.x,cir10.y,2*r);
+empty(cir11.x,cir11.y,2*r);
+base(cir12.x,cir12.y,2*r);
+base(cir13.x,cir13.y,2*r);
+empty(cir14.x,cir14.y,2*r);
+base(cir15.x,cir15.y,2*r);
+empty(cir16.x,cir16.y,2*r);}
+
+function draw2(){beginShape();
+//stroke(0);strokeWeight(1);noFill()
+fill(0)              
+
+vertex(cir5.x-r,cir5.y); 
+vertex(cir1.x-r,cir1.y); 
+
+if(r<vmin){vertex(cir1.x+r*cos(PI/4.0),cir1.y-r*sin(PI/4.0));}
+else{vertex(cir1.x+r*cos(PI/8.0),cir1.y-r*sin(PI/8.0));}
+                 
+if(r<vmin){vertex(cir11.x-r*cos(PI/4.0),cir11.y+r*sin(PI/4.0));}else{
+  vertex(cir11.x-r*cos(PI/8.0),cir11.y+r*sin(PI/8.0));
+}
+                 
+if(r<vmin){vertex(cir11.x+r*cos(PI/12.0),cir11.y+r*sin(PI/12.0));   }else{
+vertex(cir11.x+r,cir11.y);}
+
+                 
+  if(r<vmin){vertex(cir4.x-r*cos(PI/12.0),cir4.y-r*sin(PI/12.0));   }else{
+vertex(cir4.x-r,cir4.y);
+  }
+
+                 
+if(r<vmin){vertex(cir4.x+r*cos(PI/12.0),cir4.y+r*sin(PI/12.0));}else if(r>vmax){vertex(cir4.x+r*cos(PI/3.0),cir4.y+r*sin(PI/3.0));}else
+{vertex(cir4.x+r*cos(PI/4.0),cir4.y+r*sin(PI/4.0));}
+                 
+if(r<vmin){vertex(cir8.x-r*cos(PI/12.0),cir8.y-r*sin(PI/12.0));}else if(r>vmax)
+{vertex(cir8.x-r*cos(PI/3.0),cir8.y-r*sin(PI/3.0));}else
+{vertex(cir8.x-r*cos(PI/4.0),cir8.y-r*sin(PI/4.0));}
+                 
+if(r<vmin){
+  vertex(cir8.x-r*cos(PI/12.0),cir8.y+r*sin(PI/12.0));
+}else if(r>vmax)
+{vertex(cir8.x-r*cos(PI/3.0),cir8.y+r*sin(PI/3.0));}  
+else{vertex(cir8.x-r*sin(PI/4.0),cir8.y+r*cos(PI/4.0));}
+                 
+                
+if(r<vmin){ vertex(cir12.x+r*cos(PI/12.0),cir12.y-r*sin(PI/12.0)); }  else if(r>vmax)
+{vertex(cir12.x+r*cos(PI/3.0),cir12.y-r*sin(PI/3.0));}else{           
+vertex(cir12.x+r*cos(PI/4.0),cir12.y-r*sin(PI/4.0));}
+                 
+vertex(cir12.x+r*sin(PI/4.0),cir12.y+r*cos(PI/4.0));
+vertex(cir15.x+r*sin(PI/4.0),cir15.y+r*cos(PI/4.0));
+
+if(r<vmin){ vertex(cir15.x-r*sin(PI/12.0),cir15.y+r*cos(PI/12.0));}else if(r>vmax){
+vertex(cir15.x-r*sin(PI/3.0),cir15.y+r*cos(PI/3.0));
+}else{vertex(cir15.x-r*sin(PI/4.0),cir15.y+r*cos(PI/4.0));
+}
+                 
+if(r<vmin){ vertex(cir14.x+r*sin(PI/12.0),cir14.y-r*cos(PI/12.0));}else if(r>vmax){
+vertex(cir14.x+r*sin(PI/3.0),cir14.y-r*cos(PI/3.0));
+}else{vertex(cir14.x+r*sin(PI/4.0),cir14.y-r*cos(PI/4.0));
+}
+                 
+if(r<vmin){ vertex(cir14.x-r*sin(PI/12.0),cir14.y-r*cos(PI/12.0));}else if(r>vmax){
+vertex(cir14.x-r*sin(PI/3.0),cir14.y-r*cos(PI/3.0));
+}else{vertex(cir14.x-r*sin(PI/4.0),cir14.y-r*cos(PI/4.0));
+}
+                 
+if(r<vmin){ vertex(cir13.x+r*sin(PI/12.0),cir13.y+r*cos(PI/12.0));}else if(r>vmax){
+vertex(cir13.x+r*sin(PI/3.0),cir13.y+r*cos(PI/3.0));
+}else{vertex(cir13.x+r*sin(PI/4.0),cir13.y+r*cos(PI/4.0));
+}
+                 
+if(r<vmin){ vertex(cir13.x-r*sin(PI/8.0),cir13.y-r*cos(PI/8.0));}else if(r>vmax){
+vertex(cir13.x,cir13.y-r);
+}else{vertex(cir13.x-r*sin(PI/12.0),cir13.y-r*cos(PI/12.0));
+}
+if(r<vmin){ vertex(cir10.x+r*sin(PI/8.0),cir10.y+r*cos(PI/8.0));}else if(r>vmax){
+vertex(cir10.x,cir10.y+r);
+}else{vertex(cir10.x+r*sin(PI/12.0),cir10.y+r*cos(PI/12.0));
+} 
+if(r<vmin){ vertex(cir10.x+r*sin(PI/8.0),cir10.y-r*cos(PI/8.0));}else if(r>vmax){
+vertex(cir10.x,cir10.y-r);
+}else{vertex(cir10.x+r*sin(PI/12.0),cir10.y-r*cos(PI/12.0));
+} 
+if(r<vmin){ vertex(cir5.x-r*sin(PI/8.0),cir5.y+r*cos(PI/8.0));}else if(r>vmax){
+vertex(cir5.x,cir5.y+r);
+}else{vertex(cir5.x-r*sin(PI/12.0),cir5.y+r*cos(PI/12.0));
+}
+endShape(CLOSE);
+                }
+
+function refill2(){ empty(cir8.x,cir8.y,2*r);
+  empty(cir11.x,cir11.y,2*r);  
+empty(cir14.x,cir14.y,2*r);
+empty(cir10.x,cir10.y,2*r); }
 
 function grid1(){
   k=2
   for (a=65;a<=width;a+=90) {
-     for (b=65;b<=width;b+=90){
-fill(255,0,0)
+     for (b=65;b<=height;b+=90){
+fill(255,30,60)
        noStroke()
 circle(a-r,b,k)
 circle(a+r,b,k)
 circle(a,b+r,k)
 circle(a,b-r,k)
+
 circle(a+r*cos(PI/8.0),b+r*sin(PI/8.0),k)
 circle(a+r*cos(PI/8.0),b-r*sin(PI/8.0),k)
 circle(a-r*cos(PI/8.0),b+r*sin(PI/8.0),k)
 circle(a-r*cos(PI/8.0),b-r*sin(PI/8.0),k)
+
 circle(a+r*sin(PI/8.0),b-r*cos(PI/8.0),k)
 circle(a-r*sin(PI/8.0),b-r*cos(PI/8.0),k)
 circle(a-r*sin(PI/8.0),b+r*cos(PI/8.0),k)
@@ -211,10 +317,7 @@ circle(a+r*cos(PI/4.0),b-r*sin(PI/4.0),k)
 circle(a-r*cos(PI/4.0),b-r*sin(PI/4.0),k)
 circle(a-r*cos(PI/4.0),b+r*sin(PI/4.0),k)
      
-circle(a+r*cos(PI/2.0),b+r*sin(PI/2.0),k)
-circle(a+r*cos(PI/2.0),b-r*sin(PI/2.0),k)
-circle(a-r*cos(PI/2.0),b-r*sin(PI/2.0),k)
-circle(a-r*cos(PI/2.0),b+r*sin(PI/2.0),k)
+
  
      }}
 }
