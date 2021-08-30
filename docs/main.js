841be896e437ad1b6d8e2509f28fc07253615115
Constanzia <83115706+Constanzia@users.noreply.github.com>
2021-08-30 12:02:53 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -18,7 +18,7 @@ cs.style('background-color', 'white');
     cs.style('width','35px')
       cs.style('height','35px')
 cs.style('mix-blend-mode', 'exclusion');
-cs.position(mouseX, mouseY+0.8*windowHeight);
+cs.position(mouseX, mouseY+0.9*windowHeight);
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
