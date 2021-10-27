d7174ab2a2c5255ba01620b64bfd72ea66c7ab9f
Constanzia <83115706+Constanzia@users.noreply.github.com>
2021-10-27 10:55:23 +0800

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,25 +1,25 @@
-let cs 
-function setup() {
-  createCanvas(windowWidth,windowHeight);
-cs= createElement('h6', '.');
-}
+// let cs 
+// function setup() {
+//   createCanvas(windowWidth,windowHeight);
+// cs= createElement('h6', '.');
+// }
 
-function draw() {
-  background(0);
- clear();
-//blendMode(LIGHTEST);
-  fill(200)
-  noStroke()
- //circle(mouseX,mouseY,30);
+// function draw() {
+//   background(0);
+//  clear();
+// //blendMode(LIGHTEST);
+//   fill(200)
+//   noStroke()
+//  //circle(mouseX,mouseY,30);
 
-cs.style('background-color', 'white');
-  cs.style('color', 'white');
-  cs.style('border-radius','800px')
-    cs.style('width','35px')
-      cs.style('height','35px')
-cs.style('mix-blend-mode', 'exclusion');
-cs.position(mouseX, mouseY+0.95*windowHeight);
-}
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+// cs.style('background-color', 'white');
+//   cs.style('color', 'white');
+//   cs.style('border-radius','800px')
+//     cs.style('width','35px')
+//       cs.style('height','35px')
+// cs.style('mix-blend-mode', 'exclusion');
+// cs.position(mouseX, mouseY+0.95*windowHeight);
+// }
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
